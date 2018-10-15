6e824b869241f972db42f402f403450d10198357
Alobal123 <miroslavkrabec@seznam.cz>
2018-10-15 17:31:59 +0200

    Version which plays tic tac toe, but cannot check for winner and does not check validity of the moves.

--- Cviko3-piskvorky/Cviko3-piskvorky/piskvorky.h
+++ Cviko3-piskvorky/Cviko3-piskvorky/piskvorky.h
@@ -9,10 +9,28 @@ public:
 	int size;
 
 	Board(int size);
+	Board();
 
 	void make_play(int x, int y, char symbol);
 
 	void print();
 
+	bool check_for_winner();
+
+};
+
+class Game {
+	
+	
+	int turn = 0;
+	char p1_symbol = 'X';
+	char p2_symbol = 'O';
+	Board board;
+
+public:
+	void play();
+	Game();
+private:
+	void play_turn();
 
 };
