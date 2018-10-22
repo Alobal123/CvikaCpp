cbd729548408c25eb28c2cf0684a1950669ec070
Alobal123 <miroslavkrabec@seznam.cz>
2018-10-22 15:24:10 +0200

    Added controling of moves and row/column controling of winner.

--- Cviko3-piskvorky/Cviko3-piskvorky/piskvorky.h
+++ Cviko3-piskvorky/Cviko3-piskvorky/piskvorky.h
@@ -7,20 +7,18 @@ class Board {
 	std::vector<std::vector<char> > board;
 public:
 	int size;
-
 	Board(int size);
 	Board();
 
 	void make_play(int x, int y, char symbol);
-
 	void print();
-
-	bool check_for_winner();
-
+	bool check_for_winner(int limit);
+	bool is_valid_move(int x, int y);
+private:
+	bool check_for_winner_row(bool rows, int limit);
 };
 
 class Game {
-	
 	
 	int turn = 0;
 	char p1_symbol = 'X';
